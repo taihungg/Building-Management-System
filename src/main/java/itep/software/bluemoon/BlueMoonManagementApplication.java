bfeececfd41730ffb221b7c5254f189f54e1c65f
taihungg <hunginception@gmail.com>
2025-10-26 19:44:41 +0700

    init spring boot project

---
+++ src/main/java/itep/software/bluemoon/BlueMoonManagementApplication.java
@@ -0,0 +1,13 @@
+package itep.software.bluemoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueMoonManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueMoonManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itep/software/bluemoon/BlueMoonManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package itep.software.bluemoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueMoonManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
