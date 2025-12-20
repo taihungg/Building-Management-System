07ca2883f3e296ef6e4ea0cf9611878c54aafd8a
taihungg <hunginception@gmail.com>
2025-12-20 19:50:28 +0700

    edit entity User

--- src/main/java/itep/software/bluemoon/entity/User.java
+++ src/main/java/itep/software/bluemoon/entity/User.java
@@ -8,11 +8,15 @@ import org.springframework.data.annotation.CreatedDate;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import itep.software.bluemoon.entity.person.Person;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -41,12 +45,6 @@ public class User {
     @Column(nullable = false, unique = true)
     private String username;
 
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false, unique = true)
-    private String phone;
-
     @Column(nullable = false)
     @JsonIgnore
     private String password;
@@ -55,4 +53,12 @@ public class User {
     @CreatedDate
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate createDate;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "person_id",
+            referencedColumnName = "id",
+            unique = true
+    )
+    private Person person;
 }
