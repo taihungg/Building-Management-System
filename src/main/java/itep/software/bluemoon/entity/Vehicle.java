f4fd73e06660bf94b921f2b4e6f536b646397b11
taihungg <hunginception@gmail.com>
2025-12-20 20:01:43 +0700

    edit entity Vehicle

--- src/main/java/itep/software/bluemoon/entity/Vehicle.java
+++ src/main/java/itep/software/bluemoon/entity/Vehicle.java
@@ -1,11 +1,24 @@
 package itep.software.bluemoon.entity;
 
+import java.util.UUID;
+
 import itep.software.bluemoon.entity.person.Resident;
 import itep.software.bluemoon.enumeration.VehicleType;
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -25,15 +38,12 @@ public class Vehicle {
     )
     private UUID id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, length = 20)
     private String name;
 
     @Transient
-    @Column(name = "type", nullable = false)
+    @Column(name = "type", nullable = false, length = 10)
     private VehicleType type;
-
-    @Column(name = "description", columnDefinition = "TEXT")
-    private String description;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "owner_id", nullable = false)
