be22a4a8e72e3879c4f3def71494a914b8561497
taihungg <hunginception@gmail.com>
2025-12-21 17:57:59 +0700

    add method create and delete building

--- src/main/java/itep/software/bluemoon/service/BuildingService.java
+++ src/main/java/itep/software/bluemoon/service/BuildingService.java
@@ -4,10 +4,16 @@ import itep.software.bluemoon.model.projection.Dropdown;
 import itep.software.bluemoon.repository.BuildingRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
+import itep.software.bluemoon.entity.Building;
+import itep.software.bluemoon.model.DTO.apartment.BuildingCreationDTO;
+import itep.software.bluemoon.model.DTO.apartment.BuildingDetailDTO;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +27,19 @@ public class BuildingService {
         }
 
         return buildingRepository.searchForDropdown(keyword.trim());
+    }
+
+    @SuppressWarnings("null")
+    public void createBuilding(BuildingCreationDTO dto){
+        if(dto.getName() == null){
+            throw new IllegalArgumentException("Required name not null!");
+        }
+        Building newBuilding = Building.builder().name(dto.getName()).build();
+        buildingRepository.save(newBuilding);
+    }
+
+    @SuppressWarnings("null")
+    public void deleteBuilding(UUID id){
+        buildingRepository.deleteById(id);
     }
 }
