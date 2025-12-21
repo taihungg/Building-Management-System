67cdf99659de2c200f41209dc2927fbc1ef98d78
taihungg <hunginception@gmail.com>
2025-12-21 11:21:34 +0700

    add composition relation for Building and Apartment

--- src/main/java/itep/software/bluemoon/entity/Building.java
+++ src/main/java/itep/software/bluemoon/entity/Building.java
@@ -1,12 +1,17 @@
 package itep.software.bluemoon.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,4 +39,13 @@ public class Building {
 
     @Column(name = "name", nullable = false, unique = true, length = 50)
     private String name;
+
+    @OneToMany(
+        mappedBy = "building",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true,
+        fetch = FetchType.LAZY
+    )
+    @Builder.Default
+    private List<Apartment> apartments = new ArrayList<>();
 }
