67b4af7a11244ea7681c837666ce86fef211a150
taihungg <hunginception@gmail.com>
2025-12-21 12:24:11 +0700

    add status detail for ACTIVE resident

--- src/main/java/itep/software/bluemoon/entity/person/Resident.java
+++ src/main/java/itep/software/bluemoon/entity/person/Resident.java
@@ -6,17 +6,7 @@ import java.util.List;
 import itep.software.bluemoon.entity.Announcement;
 import itep.software.bluemoon.entity.Apartment;
 import itep.software.bluemoon.enumeration.ResidentStatus;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -31,8 +21,7 @@ import lombok.experimental.SuperBuilder;
 public class Resident extends Person {
     @Column(name = "status", length = 8)
     @Enumerated(EnumType.STRING)
-    @Builder.Default
-    private ResidentStatus status = ResidentStatus.ACTIVE;
+    private ResidentStatus status;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "apartment_id")
