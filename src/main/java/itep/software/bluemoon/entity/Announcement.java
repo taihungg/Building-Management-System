6f79b5bc744eb41686fa0f8c2b0d56c29c4bfbec
vu-ha <vietha26012005@gmail.com>
2025-12-22 11:38:24 +0700

    Adjust entity classes: Announcement, Resident, Staff

--- src/main/java/itep/software/bluemoon/entity/Announcement.java
+++ src/main/java/itep/software/bluemoon/entity/Announcement.java
@@ -1,11 +1,14 @@
 package itep.software.bluemoon.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
 import itep.software.bluemoon.entity.person.Staff;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -14,6 +17,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -52,4 +56,13 @@ public class Announcement {
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+    
+    @OneToMany(
+            mappedBy = "announcement", 
+            cascade = CascadeType.ALL, 
+            orphanRemoval = true,
+            fetch = FetchType.LAZY
+        )
+    @Builder.Default
+    private List<ResidentAnnouncement> residentAnnouncements = new ArrayList<>();
 }
