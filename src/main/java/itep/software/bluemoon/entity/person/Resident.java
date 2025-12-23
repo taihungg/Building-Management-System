acd15c184011c95c5fcfacb83e7f676664c4650c
taihungg <hunginception@gmail.com>
2025-12-23 20:02:59 +0700

    clear unused import

--- src/main/java/itep/software/bluemoon/entity/person/Resident.java
+++ src/main/java/itep/software/bluemoon/entity/person/Resident.java
@@ -1,12 +1,7 @@
 package itep.software.bluemoon.entity.person;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import itep.software.bluemoon.entity.Apartment;
-import itep.software.bluemoon.entity.ResidentAnnouncement;
 import itep.software.bluemoon.enumeration.ResidentStatus;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -14,11 +9,9 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
--- src/main/java/itep/software/bluemoon/entity/person/Staff.java
+++ src/main/java/itep/software/bluemoon/entity/person/Staff.java
@@ -2,21 +2,14 @@ package itep.software.bluemoon.entity.person;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import itep.software.bluemoon.entity.Announcement;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -38,13 +31,4 @@ public class Staff extends Person {
     @Column(name = "salary", precision = 19, scale = 2)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "#,##0.00")
     private BigDecimal salary;
-    
-    @OneToMany(
-        	mappedBy = "sender",
-        	cascade = CascadeType.ALL,
-        	orphanRemoval = true,
-            fetch = FetchType.LAZY
-        )
-    @Builder.Default
-    private List<Announcement> announcementsCreated = new ArrayList<>();
 }
