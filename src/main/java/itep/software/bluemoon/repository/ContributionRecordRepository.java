6cec2bd0da44858871e50ee8f3a493658ca136f0
vu-ha <vietha26012005@gmail.com>
2025-12-30 22:23:22 +0700

    Export excel voluntary contribution campaign

--- src/main/java/itep/software/bluemoon/repository/ContributionRecordRepository.java
+++ src/main/java/itep/software/bluemoon/repository/ContributionRecordRepository.java
@@ -3,19 +3,28 @@ package itep.software.bluemoon.repository;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import itep.software.bluemoon.entity.accounting.ContributionRecord;
 import itep.software.bluemoon.model.projection.ContributionSummary;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 
 @Repository
 public interface ContributionRecordRepository extends JpaRepository<ContributionRecord, UUID> {
     
-    Page<ContributionRecord> findByCampaignId(UUID campaignId, Pageable pageable);
-    
     List<ContributionSummary> findByCampaignId(UUID campaignId);
+    
+    @Query("SELECT c.id AS id, " +
+    	       "c.contributorName AS contributorName, " +
+    	       "c.phone AS phone, " +
+    	       "c.address AS address, " +
+    	       "c.amount AS amount, " +
+    	       "c.contributionDate AS contributionDate " +
+    	       "FROM ContributionRecord c " +
+    	       "WHERE c.campaign.id = :campaignId " +
+    	       "ORDER BY c.contributionDate ASC")
+    List<ContributionSummary> findByCampaignIdOrderByContributionDateAsc(@Param("campaignId") UUID campaignId);
 }
